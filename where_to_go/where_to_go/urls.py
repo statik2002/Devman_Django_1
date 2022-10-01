2657046e56a32c7e1dd5df214434bb2a0545cd9c
statik2002 <statik2002@mail.ru>
2022-10-02 00:37:22 +0800

    Add blank start page

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import show_phones
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', show_phones),
 ]
