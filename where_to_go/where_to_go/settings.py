b53b5f7594e9eddfe009c24669f51e46a94e2af7
statik2002 <statik2002@mail.ru>
2022-12-06 22:30:25 +0800

    Setup media files serving

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = 'places/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
