7533bee774500beb3c0cc69a184cf70adfe71947
statik2002 <statik2002@mail.ru>
2022-12-12 15:12:13 +0800

    add images in inlines

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Feature, FeatureImage
 
@@ -11,6 +12,16 @@ class FeatureImageAdmin(admin.ModelAdmin):
 
 class FeatureImageInline(admin.TabularInline):
     model = FeatureImage
+
+    readonly_fields = ('image_tag',)
+
+    extra = 1
+
+    def image_tag(self, obj):
+        fixed_width = 200
+        width_percent = (fixed_width / float(obj.image.width))
+        height_size = int((float(obj.image.height) * float(width_percent)))
+        return mark_safe(f'<img src="{obj.image.url}" width="{fixed_width}" height={height_size} />')
 
 
 @admin.register(Feature)
