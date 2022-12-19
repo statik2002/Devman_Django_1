fb6134fd2bfe01eb0afdeb3b94707f684d8203f0
statik2002 <statik2002@mail.ru>
2022-12-19 14:06:04 +0800

    правки c 5 по 10

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -4,7 +4,7 @@ from io import BytesIO
 from urllib.parse import urlparse
 
 import requests
-from django.core.files.images import ImageFile
+from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
 from places.models import Place, Image
 
@@ -17,29 +17,31 @@ class Command(BaseCommand):
         response.raise_for_status()
         place_from_file = response.json()
 
-        place = Place.objects.filter(title=place_from_file['title'])
-        if not place:
-            new_feature = Place.objects.create(
-                title=place_from_file['title'],
-                description_short=place_from_file['description_short'],
-                description_long=place_from_file['description_long'],
-                lng=place_from_file['coordinates']['lng'],
-                lat=place_from_file['coordinates']['lat'],
+        place, created = Place.objects.update_or_create(
+            title=place_from_file.get('title'),
+            description_short=place_from_file.get('description_short', ''),
+            description_long=place_from_file.get('description_long', ''),
+            lng=place_from_file.get('coordinates').get('lng'),
+            lat=place_from_file.get('coordinates').get('lat'),
+            defaults={'title': place_from_file.get('title')}
+        )
+
+        if not created:
+            return
+
+        for counter, img_url in enumerate(place_from_file.get('imgs', [])):
+            response = requests.get(img_url)
+            response.raise_for_status()
+
+            file_path = urllib.parse.unquote(urlparse(img_url).path)
+            path, file_name = os.path.split(file_path)
+
+            image = BytesIO(response.content)
+            Image.objects.create(
+                place=place,
+                order=counter,
+                image=ContentFile(image.read(), name=file_name)
             )
-            for counter, img_url in enumerate(place_from_file['imgs']):
-                response = requests.get(img_url)
-                response.raise_for_status()
-
-                file_path = urllib.parse.unquote(urlparse(img_url).path)
-                path, file_name = os.path.split(file_path)
-
-                image = BytesIO(response.content)
-                place_image = Image.objects.create(
-                    feature=new_feature,
-                    order=counter
-                )
-                if place_image:
-                    place_image.image.save(file_name, ImageFile(image))
 
     def add_arguments(self, parser):
         parser.add_argument('url', type=str)
